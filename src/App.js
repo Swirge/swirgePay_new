736702f438e5943040a7bd3995fb10df105aeea2
Nwafor Uchechukwu <nwaforuchechukwu2007@gmail.com>
2021-04-27 21:18:13 +0100

    Revert "about us, contact us and team players sections done"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import './App.scss';
 import { Route, BrowserRouter } from "react-router-dom";
-import TeamPlayers from './sections/team-players';
-import AboutUs from './sections/about-us';
-import ContactUs from './sections/contact-us';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/">
-        {/* <Header /> */}
-        <main>
-          <AboutUs />
-          <ContactUs/>
-          <TeamPlayers/>
-        </main>
-      </Route>
+    <Route exact path="/">
+      <main>
+        Hello Word
+      </main>
+    </Route>
   </BrowserRouter>
   );
 }
