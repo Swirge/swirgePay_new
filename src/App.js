71fae4fba22b456e0b0f968119ce235dab1206dd
udexmickey <jerrymike80@yahoo.com>
2021-04-29 13:30:38 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.scss';
-import { Route, BrowserRouter } from "react-router-dom";
+// import { Route, BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./component/ThemeProvider";
-import TeamPlayers from './sections/team-players';
-import AboutUs from './sections/about-us';
-import ContactUs from './sections/contact-us';
+// import TeamPlayers from './sections/team-players';
+// import AboutUs from './sections/about-us';
+// import ContactUs from './sections/contact-us';
 import AppRoutes from "./routes";
 
 function App() {
